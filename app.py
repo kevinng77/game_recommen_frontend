4012f1d116e9bb7e586706784354ec5f8a030626
kevinng77 <417333277@qq.com>
2021-07-05 09:31:30 +0800

    update navigator

--- app.py
+++ app.py
@@ -1,43 +1,47 @@
-from flask import Flask, request,render_template
-import json
-from datetime import datetime
+from flask import Flask, request, render_template
 import re
 import pandas as pd
 import random
 from config import config
 from utils import web_utils
+from datetime import datetime
+
 
 app = Flask(__name__)
 
-# game_url = https://store.steampowered.com/app/{ID}/{Name}
 applist = pd.read_csv(config.game_info_path)
-
-
 selected_ids = random.choices(applist.AppID, k=1000)
 msg = web_utils.gen_msg(applist, config.max_title_len, selected_ids)
 current_id = config.num_show
+
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html",buffer = msg[:config.num_show])
+    return render_template("index.html", buffer=msg[:config.num_show])
+
 
 @app.route('/loadmore')
 def load_more():
     global current_id
     outputs = ""
-    for i in range(config.num_more_line*3):
-        current_item = msg[current_id+i]
-        name,pic,price,url = current_item
-        outputs += web_utils.load_more_formatting(pic=pic,name=name,price=price,
-                                                  url= url)
-    current_id += config.num_more_line*3
+    for i in range(config.num_more_line * 3):
+        current_item = msg[current_id + i]
+        name, pic, price, url = current_item
+        outputs += web_utils.load_more_formatting(pic=pic, name=name, price=price,
+                                                  url=url)
+    current_id += config.num_more_line * 3
     return outputs
+
 
 @app.route('/click')
 def click():
     text = request.args.get('msg')
     # TODO 通过用户点击更新推荐列表
     print(text)
+    print(datetime.now())
     return 'None'
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
--- utils/web_utils.py
+++ utils/web_utils.py
@@ -1,12 +1,11 @@
-
-def process_title(title ,max_title_len):
-    if len(title ) >max_title_len:
-        return title[:max_title_len -3] + "..."
+def process_title(title, max_title_len):
+    if len(title) > max_title_len:
+        return title[:max_title_len - 3] + "..."
     else:
         return title
 
 
-def gen_msg(applist ,max_title_len ,selected_ids):
+def gen_msg(applist, max_title_len, selected_ids):
     """
     :param applist: data frame
     :param selected_ids: AppID to recommended
@@ -19,17 +18,19 @@ def gen_msg(applist ,max_title_len ,selected_ids):
     l_view = df_selected_game.Picture.tolist()
     l_url = df_selected_game.url.tolist()
     l_price = df_selected_game.final_price.tolist()
-    msg = list(zip(l_name, l_view, l_price ,l_url))
+    msg = list(zip(l_name, l_view, l_price, l_url))
     # TODO add price,
     return msg
 
-def load_more_formatting(pic ,name ,price ,url):
+
+def load_more_formatting(pic, name, price, url,tag = "todo: adding tag"):
     template = f'<div class="item">' \
                f'<div class="img_block">' \
                f'<img src="{pic}" alt="item" />' \
                f'</div><div class="name_block">' \
-               f'<h2>{name}</h2></div><div>' \
-               f'<a  class="price">Price: <em>${price}</em>' \
+               f'<h2>{name}</h2></div>' \
+               f'<div class="game_tag">{tag}</div><div>' \
+               f'<a  class="price"><em>${price}</em>' \
                f'</a><a class="details" ' \
                f'onclick="load_detail(\'{url}\',\'{name}\')">' \
                f'<em>Details>></em></a></div></div>'
